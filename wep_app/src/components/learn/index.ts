bfe7450e3aa2e960bb78e9da41ca690509becf8f
Caio Sa <caiohenriquesa1@gmail.com>
2022-04-16 20:01:18 -0300

    learning ts

--- wep_app/src/components/learn/index.ts
+++ wep_app/src/components/learn/index.ts
@@ -1,1 +1,18 @@
 console.log("Hello")
+let nome: string = "Caio"
+
+let arrayOfStrings: Array<string>
+= ['a','b']
+
+let arr : string[] = ['a']
+
+const concatenation: (x: Array<string>, y: Array<string>) => void = (x,y) => {
+    x.push(y[0])
+    console.log(x)
+}
+
+concatenation(arr,arrayOfStrings)
+
+const tuppling: [string[], number[]] = ['Joao','Thiago', 1,8]
+
+console.log(tuppling[0])
